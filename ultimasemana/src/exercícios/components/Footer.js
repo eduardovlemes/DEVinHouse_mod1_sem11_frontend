2486de8a8fed8dabd7556cc23e254a59c818776a
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-15 18:24:14 -0300

    Exercício 3

--- ultimasemana/src/exercícios/components/Footer.js
+++ ultimasemana/src/exercícios/components/Footer.js
@@ -1,23 +1,11 @@
-import { Pagination } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <Pagination>
-      <Pagination.First />
-      <Pagination.Prev />
-      <Pagination.Item>{1}</Pagination.Item>
-      <Pagination.Ellipsis />
-
-      <Pagination.Item>{10}</Pagination.Item>
-      <Pagination.Item>{11}</Pagination.Item>
-      <Pagination.Item active>{12}</Pagination.Item>
-      <Pagination.Item>{13}</Pagination.Item>
-      <Pagination.Item disabled>{14}</Pagination.Item>
-
-      <Pagination.Ellipsis />
-      <Pagination.Item>{20}</Pagination.Item>
-      <Pagination.Next />
-      <Pagination.Last />
-    </Pagination>
+    <Container>
+      <Row>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
   );
 }
