7df6b5aa4ef91c5a2aebeb6f770e7e0773845dbb
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-09-15 18:19:05 -0300

    Exercício 2

---
+++ ultimasemana/src/exercícios/components/Header.js
@@ -0,0 +1,17 @@
+import { Figure } from "react-bootstrap";
+
+export default function Header() {
+  return (
+    <Figure>
+      <Figure.Image
+        width={171}
+        height={180}
+        alt="171x180"
+        src="holder.js/171x180"
+      />
+      <Figure.Caption>
+        Nulla vitae elit libero, a pharetra augue mollis interdum.
+      </Figure.Caption>
+    </Figure>
+  );
+}
